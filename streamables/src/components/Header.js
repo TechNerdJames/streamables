94e1ccf7b72e6827e54b287939c23b9a7f39f7c5
James <jamesdryden7@gmail.com>
2022-06-30 11:34:21 -0400

    made changes to header section

--- streamables/src/components/Header.js
+++ streamables/src/components/Header.js
@@ -3,12 +3,9 @@ import React from 'react';
 function Header() {
 	return (
 		<header>
-			<span role="img" aria-label="movie icon" className="movie-icon left">
-				🎬
-			</span>
 			<h1>Streamables</h1>
 			<span role="img" aria-label="movie icon" className="movie-icon right">
-				🎞
+				🎬
 			</span>
 		</header>
 	);
